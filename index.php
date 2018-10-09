dda32c107971e6f5cb169144a81d8432dace9591
Livio <liviofeler971@outlook.fr>
2018-10-09 16:06:29 +0200

    09/10

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
-	<head></head>
+	<head>
+		<script src="sjcl/core/aes.js"></script>
+	</head>
 	<body>
 		<form method="Post" action="index.php">
 			<p>
@@ -9,6 +11,23 @@
 			</p>
 			<input type="submit" value="Envoyer" name="submit"/>
 		</form>
+	<script> 
+			var uniqid = function() {
+			return (new Date().getTime() + Math.floor((Math.random()*10000)+1)).toString(16);
+			};
+			var sjcl = require('./sjcl'); 
+			console.log(sjcl.encrypt(uniquid, message); 
+	</script>
 <html/>
+
+
 <?php
+		//Cas A
+		if(isset($_POST['submit']))
+		{
+			$id = uniqid();
+			//echo $id;
+			
+		}
+		
 ?>
