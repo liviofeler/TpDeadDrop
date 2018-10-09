39d8ce889d5612943e605c09dbb3248a39a28289
Livio <liviofeler971@outlook.fr>
2018-10-09 14:47:35 +0200

    09/10/2018

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head></head>
+	<body>
+		<form method="Post" action="index.php">
+			<p>
+				<label for="message">Message</label>
+				<input type="textarea" name="message"/>
+				<br />
+			</p>
+			<input type="submit" value="Envoyer" name="submit"/>
+		</form>
+<html/>
+<?php
+?>
